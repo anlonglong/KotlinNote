580110610d7b1bb5bd18954ae6a7bb1c24650954
Allen <940752944@qq.com>
2018-03-01 10:09:21 +0800

    完善内部类和嵌套类访问外部类的规则

--- NestClass/src/com/nest/Foo.kt
+++ NestClass/src/com/nest/Foo.kt
@@ -3,12 +3,19 @@ class Foo:Base() {
 	val f:Int = 10
 	inner class FooInner {
 		///内部类访问外部类父类中的属性.
-		val i:Int = super@Foo.b
+		val i:Int = super@Foo.b //内部类无法获取外不类的引用.
 		
 		fun ff() {
 			//内部类访问外部类父类中的方法的格式
 			super@Foo.f()
 			println("a= $f")
+		}
+	}
+	
+	class Nest {
+		//val n : Int = this@Foo.f 嵌套类无法获取外不类的引用.
+		fun nest() {
+			println("f =")
 		}
 	}
 }
